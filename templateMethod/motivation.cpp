66b2909329c4683d4d097de7a611f67ddc12ba4d
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2022-08-31 20:39:45 +1200

    update template method motivation example

--- templateMethod/motivation.cpp
+++ templateMethod/motivation.cpp
@@ -4,27 +4,46 @@ class Document
 {
 public:
 	void Open();
+	void Save();
+	void Close();
 	void DoRead();
+};
+
+class Docs
+{
+public:
+	std::vector<Document *> docs;
+	void AddDocument(Document *doc)
+	{
+		docs.push_back(doc);
+	}
 };
 
 class Application
 {
 
 public:
+	Docs *_docs = new Docs();
+	// Template Method
 	void OpenDocument(const char *name);
+	// Primitive operations - Has to be overridden (etc. abstract operations)
 	bool CanOpenDocument(const char *name);
+	// Factory Method
 	Document *DoCreateDocument();
+	// Primitive operations - Has to be overridden (etc. abstract operations)
 	void AboutToOpenDocument(Document *doc);
 };
 
+// Invariants encapsulated
 void Application::OpenDocument(const char *name)
 {
 	if (!CanOpenDocument(name))
 	{
-		// cannot handle this document
 		return;
 	}
+
 	Document *doc = DoCreateDocument();
+
 	if (doc)
 	{
 		_docs->AddDocument(doc);
