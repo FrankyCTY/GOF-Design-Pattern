4edad064d65420733c85bf6cd252a51da91fb258
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2022-09-07 20:16:03 +1200

    add adapter pattern and example for class adapter

---
+++ adapterPattern/classAdapter.cpp
@@ -0,0 +1,78 @@
+class Point
+{
+public:
+	Point(Coord lon, Coord lat);
+};
+
+// Can animate a shape when user manipulates it
+class Manipulator
+{
+};
+
+class TextManipulator : public Manipulator
+{
+public:
+	TextManipulator(const TextShape *);
+};
+
+class Coord
+{
+	friend Coord operator+(Coord &a, Coord &b)
+	{
+		return a + b;
+	}
+};
+
+// Adaptee
+class TextView
+{
+public:
+	TextView();
+	void GetOrigin(Coord &x, Coord &y) const;
+	void GetExtent(Coord &width, Coord &height) const;
+	virtual bool IsEmpty() const;
+};
+
+// Target
+class Shape
+{
+public:
+	Shape();
+	virtual void BoundingBox(
+			Point &bottomLeft, Point &topRight) const;
+
+	virtual Manipulator *CreateManipulator() const;
+};
+
+// Adapter
+class TextShape : public Shape, private TextView
+{
+public:
+	TextShape();
+	virtual void BoundingBox(
+			Point &bottomLeft, Point &topRight) const;
+	virtual bool IsEmpty() const;
+	virtual Manipulator *CreateManipulator() const;
+};
+
+// Convert TextView's interface to conform to Shape's
+void TextShape::BoundingBox(
+		Point &bottomLeft, Point &topRight) const
+{
+	Coord bottom, left, width, height;
+	GetOrigin(bottom, left);
+	GetExtent(width, height);
+	bottomLeft = Point(bottom, left);
+	topRight = Point(bottom + height, left + width);
+};
+
+// We can directly forward the request to the adpatee
+bool TextShape::IsEmpty() const
+{
+	return TextView::IsEmpty();
+}
+
+Manipulator *TextShape::CreateManipulator() const
+{
+	return new TextManipulator(this);
+}
