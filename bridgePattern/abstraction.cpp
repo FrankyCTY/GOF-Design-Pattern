85133a991f6b637508794458af8c6abb53e10073
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2022-09-14 19:35:38 +1200

    update bridge pattern cpp example

--- bridgePattern/abstraction.cpp
+++ bridgePattern/abstraction.cpp
@@ -1,91 +1,39 @@
-#include "common.h"
+// ============= Refined Abstraction =============#include "common.h"
 #include "implementation.h"
+#include "./abstraction.h"
 #include <iostream>
 
-class View
+class ImplementationFactory
 {
 public:
-	void DrawOn(Window *w);
-};
-
-// Abstraction
-class Window
-{
-public:
-	Window(View *contents);
-	// requests handled by window
-	virtual void DrawContents();
-	virtual void Open();
-	virtual void Close();
-	virtual void Iconify();
-	virtual void Deiconify();
-
-	// requests forwarded to implementation
-	virtual void SetOrigin(const Point &at);
-	virtual void SetExtent(const Point &extent);
-	virtual void Raise();
-	virtual void Lower();
-
-	virtual void DrawLine(const Point &, const Point &);
-	virtual void DrawRect(const Point &, const Point &);
-	virtual void DrawPolygon(const Point[], int n);
-	virtual void DrawText(const char *, const Point &);
-
-protected:
-	// Implementation is hidden from client
-	WindowImp *GetWindowImp();
-	View *GetView();
+	static WindowImp *CreateImplementation()
+	{
+		return new PMWindowImp();
+	}
 
 private:
-	WindowImp *_imp;
-	View *_contents;
+	ImplementationFactory(){};
 };
 
-WindowImp *Window::GetWindowImp()
+// ============= Abstraction =============
+Window::Window()
 {
-	// if (_imp == 0)
-	// {
-	// 	// Factory method (static)
-	// 	// _imp = WindowSystemFactory::Instance()->MakeWindowImp();
-	// }
-	return _imp;
+	_imp = ImplementationFactory::CreateImplementation();
 }
 
-void Window::DrawRect(const Point &p1, const Point &p2)
+void Window::DrawRect()
 {
-	WindowImp *imp = GetWindowImp();
-	imp->DeviceRect(p1.X(), p1.Y(), p2.X(), p2.Y());
+	_imp->DeviceRect();
 }
 
-class ApplicationWindow : public Window
-{
-public:
-	// . . .
-	virtual void DrawContents();
-};
-
-// Refined Abstraction
+// ============= Refined Abstraction =============
 void ApplicationWindow::DrawContents()
 {
-	GetView()->DrawOn(this);
+	std::cout << "Draw contents for ApplicationWindow" << std::endl;
 }
 
-class IconWindow : public Window
-{
-public:
-	// ...
-	virtual void DrawContents();
-
-private:
-	const char *_bitmapName;
-};
-
-// Refined Abstraction
+// ============= Refined Abstraction =============
 void IconWindow::DrawContents()
 {
-	WindowImp *imp = GetWindowImp();
-	if (imp != 0)
-	{
-		imp->DeviceBitmap(_bitmapName, 0.0, 0.0);
-	}
+	std::cout << "Draw contents for IconWindow" << std::endl;
 }
